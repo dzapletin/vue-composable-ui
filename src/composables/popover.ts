31332ed4a1c37875c4f74e9a46a5e374944250e8
Dmitry Zapletin <129387793+dzapletin@users.noreply.github.com>
2026-06-20 17:46:27 +0300

    fix(usePopover): flush calc post rendering

--- src/composables/popover.ts
+++ src/composables/popover.ts
@@ -284,7 +284,7 @@ export function usePopover(
           });
         }
       },
-      { flush: "pre" },
+      { flush: "post" },
     );
 
     window.addEventListener("resize", calcPosition, {
